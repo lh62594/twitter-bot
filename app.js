987f19c6a2abe2f6666f1f13754bfdc80f929446
github username <github email address>
2019-03-06 13:07:04 -0500

    starting

---
+++ app.js
@@ -0,0 +1,3 @@
+var Twitter = require('twitter');
+var config = require('./config.js');
+var T = new Twitter(config);
---
+++ config.js
@@ -0,0 +1,8 @@
+// we will need the data from the Twitter API
+
+// module.exports = {
+//   consumer_key: '',
+//   consumer_secret: '',
+//   access_token_key: '',
+//   access_token_secret: ''
+// }
